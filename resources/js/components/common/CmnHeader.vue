0106e237ef9d842c148072b3175b8e722021419a
HayataSato <hayatasato.0@gmail.com>
2021-10-31 01:36:49 +0900

    仮表示用のSFC作成

---
+++ resources/js/components/common/CmnHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="">
+    <router-link to="/">予約フォーム</router-link>
+    <router-link to="/management">管理画面</router-link>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'CmnHeader',
+  components: {
+  },
+};
+</script>
+
+<style>
+</style>
