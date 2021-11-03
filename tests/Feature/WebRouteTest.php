e1ec5b8ee5319d6e15c357b7e71ef11210757e06
HayataSato <hayatasato.0@gmail.com>
2021-11-03 15:53:49 +0900

    webコントローラーのテスト追加

---
+++ tests/Feature/WebRouteTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class WebRouteTest extends TestCase
+{
+    /**
+     * ルートへのアクセス
+     *
+     * @return void
+     */
+    public function testRequestRoot()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * ルート以外へのアクセス
+     *
+     * @return void
+     */
+    public function testRequestHoge()
+    {
+        $response = $this->get('/hoge');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * ルート以外へのアクセス 階層を深くする
+     *
+     * @return void
+     */
+    public function testRequestHogeSlashHoge()
+    {
+        $response = $this->get('/hoge/hoge');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * ブランクへのアクセス
+     *
+     * @return void
+     */
+    public function testRequestBlank()
+    {
+        $response = $this->get('');
+
+        print($response->path);
+        $response->assertStatus(200);
+    }
+}
