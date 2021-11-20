6dc8be4da15b8fc52b7d058124a732b9944287f7
HayataSato <hayatasato.0@gmail.com>
2021-11-20 11:35:38 +0900

    コースのserializer, responderを実装

---
+++ app/Http/Responder/CourseResponder.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Responder;
+
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Illuminate\Support\Collection;
+use RsvForm\Domain\Models\Course\Course;
+use RsvForm\Presentation\CourseJsonSerializer;
+
+class CourseResponder
+{
+    /**
+     * @var CourseJsonSerializer
+     */
+    private $serializer;
+
+    /**
+     * @param CourseJsonSerializer $serializer
+     */
+    public function __construct(CourseJsonSerializer $serializer)
+    {
+        $this->serializer = $serializer;
+    }
+
+    /**
+     * エンティティでレスポンス
+     * @param Course $course
+     * @param int $status
+     * @return JsonResponse
+     */
+    public function withEntity(Course $course, $status = Response::HTTP_OK): JsonResponse
+    {
+        return new JsonResponse(
+            [
+                'course' => $this->serializer->serialize($course)
+            ],
+            $status
+        );
+    }
+
+    /**
+     * エンティティのコレクションでレスポンス
+     * @param Collection|Course[] $courses
+     * @param int $status
+     * @return JsonResponse
+     */
+    public function withEntityCollection(Collection $courses, $status = Response::HTTP_OK): JsonResponse
+    {
+        return new JsonResponse(
+            [
+                'courses' => $this->serializer->serializeCollection($courses)
+            ],
+            $status
+        );
+    }
+
+    /**
+     * エラーのレスポンス
+     * @param Exception $e
+     * @param int $status
+     * @return JsonResponse
+     */
+    public function error(Exception $e, $status = Response::HTTP_INTERNAL_SERVER_ERROR): JsonResponse
+    {
+        return new JsonResponse(
+            [
+                'message' => $e->getMessage(),
+                'code' => $e->getCode(),
+            ],
+            $status
+        );
+    }
+}
---
+++ packages/RsvForm/Presentation/CourseJsonSerializer.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace RsvForm\Presentation;
+
+use Illuminate\Support\Collection;
+use RsvForm\Domain\Models\Course\Course;
+
+/**
+ * JsonResponseのためのシリアライザ
+ * @package N1215\LaraTodo\Presentations
+ */
+class CourseJsonSerializer
+{
+    /**
+     * @param Course $course
+     * @return array
+     */
+    public function serialize(Course $course): array
+    {
+        return [
+            'id' => $course->getId(),
+            'name' => $course->getName(),
+            'price' => $course->getPrice(),
+            'capacity' => $course->getCapacity(),
+            'location' => $course->getLocation(),
+            'description' => $course->getDescription(),
+            'isFinished' => $course->getIsFinished(),
+        ];
+    }
+
+    /**
+     * @param Collection|Course[] $courses
+     * @return array
+     */
+    public function serializeCollection(Collection $courses): array
+    {
+        return array_map(
+            function (Course $course) {
+                return $this->serialize($course);
+            },
+            $courses->all()
+        );
+    }
+}
