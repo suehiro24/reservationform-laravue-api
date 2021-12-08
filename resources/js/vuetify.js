9d0ba8bac5c4cac69be817ccf3e38853420c6c93
HayataSato <hayatasato.0@gmail.com>
2021-12-09 00:50:10 +0900

    googleオリジナルのiconも試せるようにする

--- resources/js/vuetify.js
+++ resources/js/vuetify.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+// Material Design Icons
+import '@mdi/font/css/materialdesignicons.css'
+// Material Icons
+// import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.use(Vuetify)
 
-export default new Vuetify()
+export default new Vuetify({
+  icons: {
+    iconfont: 'mdi',
+    // iconfont: 'md',
+  },
+})
