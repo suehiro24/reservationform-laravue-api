449e09bf2408b5bbfeca231ad2ec9e9999a31a80
HayataSato <hayatasato.0@gmail.com>
2021-12-18 11:40:21 +0900

    plugin内にstoreのmoduleを移動

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import loader from '@/store/modules/loader'
+import loader from '@/plugins/common/axios-store-loading-setter/store'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   strict: process.env.NODE_ENV !== 'production',
   modules: {
-    loader: loader,
+    loader,
   },
 })
 
