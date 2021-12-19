2c6d66eaf8b968e6c36ccddc167a3f55353c5a95
HayataSato <hayatasato.0@gmail.com>
2021-12-20 02:36:10 +0900

    Fix #13 ロード中フラグの遅延更新が他の通信実行後に来る場合を考慮

--- resources/js/plugins/common/axios-store-loading-setter/store.js
+++ resources/js/plugins/common/axios-store-loading-setter/store.js
@@ -3,6 +3,7 @@ const modules = {
 
 const state = {
   isLoading: false,
+  counter: 0,
 }
 
 const getters = {
@@ -11,10 +12,19 @@ const getters = {
 const actions = {
   updateIsLoading (context, isLoading) {
     if (isLoading) {
+      context.commit('increment')
       context.commit('setIsLoading', isLoading)
+      console.log('Incremented calling api counter', context.state.counter)
     } else {
       // falseに切り替わった場合は即時に次の通信が始まる可能性があるので, 特定ミリ秒待ってから切り替える
-      setTimeout(() => context.commit('setIsLoading', isLoading), 500, isLoading)
+      setTimeout(() => {
+        context.commit('decrement')
+        console.log('Decremented calling api counter', context.state.counter)
+        if (context.state.counter === 0) {
+          context.commit('setIsLoading', isLoading)
+          console.log('Finish loding')
+        }
+      }, 500)
     }
   },
 }
@@ -22,6 +32,12 @@ const actions = {
 const mutations = {
   setIsLoading (state, isLoading) {
     state.isLoading = isLoading
+  },
+  increment (state) {
+    state.counter++
+  },
+  decrement (state) {
+    state.counter--
   },
 }
 
